0e4b682279e15ae2b69e2c19d14758e2db64cf49
fuchsroey <fuchsroey@gmail.com>
2020-08-16 14:13:36 +0300

    svm and ann

--- ANN.py
+++ ANN.py
@@ -6,11 +6,13 @@ import torch.nn.functional as F
 class ANN(nn.Module):
     def __init__(self):
         super(ANN, self).__init__()
-        self.fc1 = nn.Linear(7, 3)
-        self.fc2 = nn.Linear(3, 2)
+        self.fc1 = nn.Linear(7, 10)
+        self.fc2 = nn.Linear(10, 4)
+        self.fc3 = nn.Linear(4,2)
 
     def forward(self, x):
         x = x.view(-1, 7)
         out = F.relu(self.fc1(x))
-        out = self.fc2(out)
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
         return F.log_softmax(out, dim=1)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.io import arff #convert from artff to csv
 from utils import remove_example_id, split_x_y, remove_unnecessary_features,split_k,shuffel, plot_data
 from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from Q3 import q3
 import os # to get working directory
@@ -29,17 +30,40 @@ data_x, data_y = split_x_y(raw_data) # split features and labels
 q3(data_x, data_y, TEST_SET_SIZE)
 '''
 
+scaler = StandardScaler()
+scaler.fit(data_x)
+data_x = scaler.transform(data_x)
+
 train_data_x, test_data_x, train_data_y, test_data_y = train_test_split(data_x, data_y, test_size= TEST_SET_SIZE,
                                                                         shuffle=True) # split data to traning and
 # test set
 
 
 # SVM
-'''for i in range(5):
-    clf = svm.SVC(C=0.1, kernel='rbf',gamma=0.1, max_iter=5000) # c = 0 -> no penalety
-    # clf.fit(train_data_x, train_data_y)
-    result = cross_validate(clf, train_data_x, train_data_y, cv=5)
-    print(np.sum(result['test_score'])/len(result['test_score']))'''
+# degree, gamma = ha'kafol (gamma * x_i * x_j), r = (b independedt), c = regulariztion
+train_x_cross, train_y_cross = split_k(train_data_x, train_data_y, 5)
+
+for i in range(5):
+
+    ## k fold
+    x = []
+    y = []
+    [x.append(train_x_cross[j].copy()) for j in range(5) if j != i]
+    [y.append(train_y_cross[j].copy()) for j in range(5) if j != i]
+    x_train = np.concatenate(np.array(x))
+    y_train = np.concatenate(np.array(y))
+
+    x_vald = np.array(train_x_cross[i])
+    y_vald = np.array(train_y_cross[i])
+
+
+    clf = svm.SVC(C=1, kernel='poly',degree=(i+1), max_iter=20000, verbose=2) # c = 0 -> no penalety
+    a = clf.fit(x_train, y_train)
+    y_hat = clf.predict(x_vald)
+    print(np.sum(y_hat == y_vald)/len(y_vald))
+    #result = cross_validate(clf, train_data_x, train_data_y, cv=5, scoring='accuracy', return_train_score=True)
+    #print(i+1)
+    #print(np.sum(result['test_score'])/len(result['test_score']))
 
 
 # ANN
@@ -67,8 +91,8 @@ for i in range(5):
     epochs = 100
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    model.train()
     for ep in range(epochs):
+        model.train()
         print("-----------", ep)
         x_train, y_train = shuffel(x_train, y_train)
         for example_x, example_y in zip(x_train, y_train):
@@ -79,28 +103,16 @@ for i in range(5):
             optimizer.step()
 
         sum = 0
+        loss_sum = 0
+        model.eval()
         for example_x, example_y in zip(x_vald, y_vald):
             output = model(torch.from_numpy(example_x).float())
+            loss = F.nll_loss(output, torch.LongTensor([example_y]), reduction='sum')
+            loss_sum += loss.item()
             if output.max(1, keepdim=True)[1] == example_y:
                 sum = sum + 1
         print(sum/len(y_vald))
+        print(loss_sum/len(y_vald))
 
 
-
-'''
-sum = 0
-model.eval()
-sumZ=0
-sumO = 0
-
-
-    if output.max(1, keepdim=True)[1] == 0:
-        sumZ = sumZ+1
-    else:
-        sumO = sumO +1
-
-
-print(sumO)
-print(sumZ)
-print(sum/len(test_data_y))'''
 
