5efb5ccb65117ecb3e1cb7b3c365aef15bf768d9
fuchsroey <fuchsroey@gmail.com>
2020-08-23 12:04:06 +0300

    ange regularizion

--- SVM.py
+++ SVM.py
@@ -5,16 +5,17 @@ from sklearn.svm import SVC
 from utils import plot_train_vald, get_reg_title
 import sys
 
-NO_REGULARIZATION = sys.float_info.max  # the biggest number that can fit to float
+NO_REGULARIZATION = 150
+
 
 def with_cross_validation(train_data_x, train_data_y, regularization=False):
-    c = 500000 if regularization else NO_REGULARIZATION
+    c = 1 if regularization else NO_REGULARIZATION
     percentage = [0.2, 0.4, 0.6, 0.8, 1]  # will learn with 20%, 40% ,.... 100% of the data
     acc_train = []
     acc_vald = []
     samples_num = [int(x * len(train_data_x) * (4 / 5)) for x in percentage]
     for k in samples_num:
-        clf = svm.SVC(C=c, kernel='rbf')  # creat svm model
+        clf = clf = svm.SVC(C=c, kernel='poly', degree=1, gamma=1, coef0=0, max_iter=2000000)  # creat svm model
         result = cross_validate(clf, train_data_x[:k], train_data_y[:k], cv=5, scoring='accuracy',
                                 return_train_score=True)  # using a part of the data every iteration.
         acc_vald.append(round(np.sum(result['test_score']) / len(result['test_score']), 3))
@@ -25,7 +26,7 @@ def with_cross_validation(train_data_x, train_data_y, regularization=False):
 
 
 def without_cross_validation(train_data_x, train_data_y, regularization=False):
-    c = 50000 if regularization else NO_REGULARIZATION
+    c = 1 if regularization else NO_REGULARIZATION
     training_set_size = int((4 / 5) * len(train_data_x))
     vald_x = train_data_x[training_set_size + 1:]  # separate to validation set
     vald_y = train_data_y[training_set_size + 1:]
@@ -36,7 +37,7 @@ def without_cross_validation(train_data_x, train_data_y, regularization=False):
     acc_vald = []
     sampels_num = [int(x * len(train_data_x)) for x in percentage]
     for k in sampels_num:
-        clf = svm.SVC(C=c, kernel='rbf')  # creat svm model
+        clf = svm.SVC(C=c, kernel='poly', degree=1, gamma=1, coef0=0, max_iter=2000000)  # creat svm model
 
         clf.fit(train_data_x[:k], train_data_y[:k])  # train the model
         y_hat = clf.predict(vald_x[:int(k / 5)])  # predict of the validation set
